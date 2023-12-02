ccfeb61a4ac83b70f30cdd2613f83bb1f066e6f3
Trofimov Ilya <guardianshield.cpp@gmail.com>
2023-12-02 17:37:39 +0300

    workpiece

---
+++ src/Board.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class Board {
+
+    int weigh;
+    int height;
+    Map<Key, Integer> board = new HashMap<>();
+
+    protected Board(int weigh, int height) {
+        this.weigh = weigh;
+        this.height = height;
+    }
+
+    abstract void fillBoard(List<Integer> list);
+    abstract List<Key> availableSpace();
+    abstract void addItem(Key key, Integer value);
+    abstract Key getKey(int i, int j);
+    abstract Integer getValue(Key key);
+    abstract List<Key> getColumn(int j);
+    abstract List<Key> getRow(int i);
+    abstract boolean hasValue(Integer value);
+    abstract List<Integer> getValues(List<Key> keys);
+
+}
